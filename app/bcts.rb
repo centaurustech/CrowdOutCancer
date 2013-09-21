8dce05fca15529f068d875866b644a40f393b6cf
Sam Salisbury <samsalisbury@gmail.com>
2013-09-21 21:13:27 +0100

    Compass files

--- app/bcts.rb
+++ app/bcts.rb
@@ -1,5 +1,20 @@
 class BCTS < Sinatra::Base
 
+	configure do
+	  set :haml, {:format => :html5, :escape_html => true}
+	  set :scss, {:style => :compact, :debug_info => false}
+	  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.rb'))
+	end
+
+	get '/stylesheets/:name.css' do
+	  content_type 'text/css', :charset => 'utf-8'
+	  scss(:"stylesheets/#{params[:name]}" )
+	end
+
+	get '/' do
+	  haml :index
+	end
+
 	get '/' do
 		
 		haml :index
